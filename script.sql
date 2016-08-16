f737fb967ddd9e9d4f58a442c0b85400d5c3cc92
cheng.liu <cheng.liu@servicenow.com>
2016-08-16 14:15:42 -0700

    Updated war and sql script for images

--- script.sql
+++ script.sql
@@ -1,4 +1,4 @@
-create DATABASE IF NOT EXISTS vegan_donuts; 
+create DATABASE IF NOT EXISTS vegan_donuts;
 
 use vegan_donuts;
 
@@ -12,13 +12,13 @@ create table  IF NOT EXISTS menu (
          price FLOAT
 );
 
-insert into menu values(1,  "Dad Donut","","http://thumb9.shutterstock.com/display_pic_with_logo/774346/251965456/stock-photo-donut-with-sprinkles-isolated-on-white-background-251965456.jpg",1);
-insert into menu values(2,  "Apple Crumb Donut","","http://thumb1.shutterstock.com/display_pic_with_logo/86931/107698130/stock-photo-group-of-glazed-donuts-on-white-background-107698130.jpg",1);
-insert into menu values(3,  "Blue Berry Crumb Donut","","http://thumb1.shutterstock.com/display_pic_with_logo/1146431/148452158/stock-photo-donut-with-pink-and-yellow-isolated-on-a-white-background-148452158.jpg",1);
-insert into menu values(4,  "Boston Scream Donut","","http://thumb9.shutterstock.com/display_pic_with_logo/74538/170315462/stock-photo-donut-with-sprinkles-isolated-on-white-background-170315462.jpg",1);
-insert into menu values(5,  "Coconut Donut","","http://thumb9.shutterstock.com/display_pic_with_logo/55199/217151596/stock-photo-morning-breakfast-with-donuts-and-honey-217151596.jpg",1);
-insert into menu values(6,  "Cinnamon Stick","","http://thumb101.shutterstock.com/display_pic_with_logo/641266/108762113/stock-photo-donut-isolated-on-white-background-108762113.jpg",1);
-insert into menu values(7,  "Coffee Roll","","http://thumb7.shutterstock.com/display_pic_with_logo/158116/148835189/stock-photo-tasty-purple-donut-isolated-on-white-148835189.jpg",1);
+insert into menu values(1,  "Dad Donut","","images/DSC01227.jpg",1);
+insert into menu values(2,  "Apple Crumb Donut","","images/DSC01228.jpg",1);
+insert into menu values(3,  "Blue Berry Crumb Donut","","images/DSC01229.jpg",1);
+insert into menu values(4,  "Boston Scream Donut","","images/DSC01230.jpg",1);
+insert into menu values(5,  "Coconut Donut","","images/DSC01231.jpg",1);
+insert into menu values(6,  "Cinnamon Stick","","images/DSC01232.jpg",1);
+insert into menu values(7,  "Coffee Roll","","images/DSC01233.jpg",1);
 
 
 commit;
